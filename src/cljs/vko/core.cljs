6fd8db23b218aa910f87c707fee965b619e34551
airnsk <airnsk@mail.ru>
2016-11-24 01:26:40 +0300

    nav

--- src/cljs/vko/core.cljs
+++ src/cljs/vko/core.cljs
@@ -13,6 +13,44 @@
 ;; Views
 
 
+(defn side-nav []
+  [:div.mdl-layout.mdl-js-layout.mdl-layout--fixed-header
+  [:header.mdl-layout__header
+  [:div.mdl-layout__header-row
+   [:span.mdl-layout-title "title"]
+   [:div.mdl-layout-spacer]
+   [:nav.mdl-navigation.mdl-layout--large-screen-only
+    [:a.mdl-navigation__link {:href "#/"} "Main page"]
+    [:a.mdl-navigation__link {:href "#/about"} "About"]
+    [:a.mdl-navigation__link {:href "#/settings"} "Settings"]]]]
+
+    [:div.mdl-layout__drawer
+    [:span.mdl-layout-title "titel2" ]
+    [:nav.mdl-navigation
+     [:a.mdl-navigation__link {:href "#/"} "Main page"]
+     [:a.mdl-navigation__link {:href "#/about"} "About"]
+     [:a.mdl-navigation__link {:href "#/settings"} "Settings"]]
+
+
+    ]
+    ])
+
+
+    (defn search-bar []
+      [:form
+       {:action "#"}
+       [:div.mdl-textfield.mdl-js-textfield.mdl-textfield--expandable
+        [:label.mdl-button.mdl-js-button.mdl-button--icon
+         {:for "search"}
+         [:i.material-icons
+          "search"]]
+        [:div.mdl-textfield__expandable-holder
+         [:input#search.mdl-textfield__input
+          {:type "text"}]
+         [:label.mdl-textfield__label
+          {:for "sample-expandable"}
+          "Expandable Input"]]]])
+
 
 (def userauth (atom true))
 
@@ -23,6 +61,9 @@
   [:button.fixed-bottom-fab.mdl-button.mdl-js-button.mdl-button--fab.mdl-js-ripple-effect.mdl-button--colored
       [:i.material-icons "add"]]
 
+
+(side-nav)
+(search-bar)
 
 
      ] )
